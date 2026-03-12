1f5abc514cbc80b2f0902fb438003cacecbf15d4
dvit1711-hi <vietnd1711@gmai.com>
2026-03-12 17:34:32 +0700

    Thêm @NotBlank, @Size cho Task

--- src/main/java/com/example/tuan3_tts_th04395/Entity/Task.java
+++ src/main/java/com/example/tuan3_tts_th04395/Entity/Task.java
@@ -2,6 +2,8 @@ package com.example.tuan3_tts_th04395.Entity;
 
 import com.example.tuan3_tts_th04395.Entity.enums.TaskStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -23,11 +25,13 @@ public class Task {
     @Column(name = "task_id", nullable = false)
     private Integer taskId;
 
+    @NotBlank(message = "Title cannot be empty")
     @Size(max = 150)
     @Column(name = "title", nullable = false, length = 150)
     private String title;
 
     @Lob
+    @Size(max = 500, message = "Description too long")
     @Column(name = "description")
     private String description;
 
@@ -40,6 +44,7 @@ public class Task {
     private String priority;
 
     @Column(name = "due_date")
+    @Future(message = "Deadline must be in the future")
     private LocalDate dueDate;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
