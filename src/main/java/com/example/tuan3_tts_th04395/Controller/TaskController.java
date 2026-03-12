12c9ad8c00586c95b3ccf39f7b2532bdf78ed60b
dvit1711-hi <vietnd1711@gmai.com>
2026-03-12 13:11:41 +0700

    API Tạo Task + API Assign Task cho User + API Update Status Task

--- src/main/java/com/example/tuan3_tts_th04395/Controller/TaskController.java
+++ src/main/java/com/example/tuan3_tts_th04395/Controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.tuan3_tts_th04395.Controller;
 
 import com.example.tuan3_tts_th04395.Entity.Task;
+import com.example.tuan3_tts_th04395.Entity.enums.TaskStatus;
 import com.example.tuan3_tts_th04395.Service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -32,5 +33,21 @@ public class TaskController {
     @PostMapping
     public Task create(@RequestBody Task task) {
         return taskService.create(task);
+    }
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+    @PutMapping("/{taskId}/assign/{userId}")
+    public Task assignTask(@PathVariable Integer taskId,
+                           @PathVariable Integer userId) {
+
+        return taskService.assignTask(taskId, userId);
+    }
+    @PutMapping("/{taskId}/status")
+    public Task updateStatus(@PathVariable Integer taskId,
+                             @RequestParam TaskStatus status) {
+
+        return taskService.updateStatus(taskId, status);
     }
 }
